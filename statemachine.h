3e619c2c47aa3e56f93053880b3fa42896c10adb
Sondre <sondrehf@ntnu.stud.no>
2018-03-07 13:47:37 +0100

    deleted extra files

--- statemachine.h
+++
@@ -1,8 +0,0 @@
-#include "utilities.h"
-#include "elev.h"
-#include "bestilling.h"
-#include <stdlib.h>
-typedef enum States{
-INITIALIZE, DRIVE, DOOR, TAKEORDER, FAIL
-}State;
-void stateMachine(State next_state, elev_motor_direction_t dir);
